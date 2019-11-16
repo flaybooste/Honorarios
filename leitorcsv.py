6addc590501b3e5b9b12c0776d6b29f8b0f85f05
flaybooste <retardado.tw@gmail.com>
2019-11-16 13:52:44 -0300

    alteração em potencial

--- leitorcsv.py
+++ leitorcsv.py
@@ -1,23 +1,26 @@
 import csv
 from db import dbSQL
 from pprint import pprint
+import pdb
 
 def inserirSQL():
-	with open('completa.csv') as _f:
+	with open('completa.CSV',encoding='ISO-8859-1') as _f:
 		linhas = csv.reader(_f)
 		x = 0
 		d0 = []
 		for linha in linhas:
-			if(linha[4] != ""):
-				pprint(linha[4])
-			if(linha[0] != "" and linha[0] != 'Relatório Completo de Empresas'):
-				try:	
-					news = linha[0].replace('(',',').replace(')', ',')
-					d0.append(news.split(','))
-					#dbSQL().escreverSQL(d0[x][1], d0[x][0])
-#					print(d0[x][1] in d1)
-					x+=1
-				except:
-					pass
+                    #pdb.set_trace()
+                    print(linha[0].strip())
+                    #if(linha[4] != ""):
+                    #       pprint(linha[4])
+                    if(linha[0] != "" and linha[0] != 'Relatório Completo de Empresas'):
+                        try:	
+                            news = linha[0].replace('(',',').replace(')', ',')
+                            d0.append(news.split(','))
+                            #dbSQL().escreverSQL(d0[x][1], d0[x][0])
+                    #        print(d0[x][1] in d1)
+                            x+=1
+                        except:
+                            pass
 		#print(d1.values())
 	#return d2
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, render_template, jsonify, request
 from db import dbSQL
 import json
+import os
 
-app = Flask('honorarios', template_folder='./static')
+
+app = Flask('honorarios', template_folder='./static', static_folder='./static')
 
 @app.route("/", methods=['GET','POST'])
 def index():
@@ -10,20 +13,29 @@ def index():
 
 @app.route("/honorarios")
 def hon():
-	empID = dbSQL().selectSQL()
-	jsn = json.dumps(empID)
-	return render_template('hon.html', ids = json.loads(jsn))
+        empID = dbSQL().selectSQL()
+        jsn = json.dumps(empID)
+        return render_template('hon.html',ids = json.loads(jsn))
 
 @app.route("/cadastro_empresas", methods=['GET','POST'])
 def cadastro():
-	if request.method == "POST":
-		dbSQL().updateTblValor(round(float(request.form['valor']),2), request.form['id'])
-	empID = dbSQL().selectSQL()
-	return render_template('empresas.html' , empresas=empID)
+        if request.method == "POST":
+            print(request.form['valor'].replace(',','.'))
+            dbSQL().updateTblValor(round(float(request.form['valor'].replace(",","."),2), request.form['id']))
+        empID = dbSQL().selectSQL()
+        return render_template('empresas.html' , empresas=empID)
 
 @app.route("/empresa/<id>", methods=['GET',' POST'])
 def empresa(id):
 	emp = dbSQL().selectId(id)
 	return render_template('empresa.html', emp=emp)
 
-app.run(debug=True)
+@app.route("/teste")
+def teste():
+    return render_template('barbudo.html')
+
+@app.route("/paty")
+def paty():
+    return 'te amo muito'
+
+app.run(port=5000, host='0.0.0.0',debug=True)
